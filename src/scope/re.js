f1e80063c39e99596a598aaf797ae9514988c62d
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-04-23 20:57:42 -0500

    scope

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // undefined
+firstName = 'Diego';
+console.log(firstName);
+
+var lastName = 'Felipe'; // Declarar / Asignar
+lastName = 'Ana'; // Reasignar
+console.log(lastName);
+
+var secondName = 'Felipe'; // Declarar / Asignar
+var secondName = 'Ana'; //Reasignar
+console.log(secondName);
+
+// let
+
+let fruit = 'Apple'; // Declarar / Asignar
+fruit = 'Kiwi'; // Reasignar
+console.log(fruit);
+
+let fruit = 'Banana'
+
+// const
+
+const animal = 'dog'; // Declar / Asignar
+animal = 'cat'; // Reasignar
+const animal = 'snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('ford');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
