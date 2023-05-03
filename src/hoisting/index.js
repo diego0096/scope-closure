751ab7d0c557380d390d9b9fecc94a0cb16de878
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-05-02 21:28:12 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+// console.log(nameOfDog); // Undefined
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog); // 'Elmo'
+
+var elmo; // Undefined
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
